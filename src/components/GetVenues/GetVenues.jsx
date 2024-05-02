f1d84e67b4fdfad24e66a6c82d6f94dc9f99a93a
maleneivy <68667843+maleneivy@users.noreply.github.com>
2024-05-02 11:58:32 +0200

    Fixed some styling issues, added maxGuests to VenueCard

--- src/components/GetVenues/GetVenues.jsx
+++ src/components/GetVenues/GetVenues.jsx
@@ -22,7 +22,7 @@ const GetVenues = () => {
     }, []);
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-4 mx-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-4 px-5">
             {venues.map(venue => (
                <VenueCard key={venue.id} venue={venue}> </VenueCard>
             ))}
--- src/components/VenueCard/VenueCard.jsx
+++ src/components/VenueCard/VenueCard.jsx
@@ -5,22 +5,25 @@ const VenueCard = ({ venue }) => {
     const imageAlt = venue.media && venue.media.length > 0 && venue.media[0].alt ? venue.media[0].alt : "Venue image";
 
     return (
-        <div className="max-w-sm rounded shadow-lg">
+        <div className="w-full max-w-sm rounded shadow-lg">
             <img className="w-full h-48 object-cover" src={imageUrl} alt={imageAlt} onError={(e) => {
                 e.target.onerror = null;
                 e.target.src = defaultImage;
             }}/>
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">
-                    <p className="text-base">
-                        {venue.description}
+                    <h3 className="text-base">
+                        {venue.name}
+                    </h3>
+                    <p className="text-sm">
+                        Max guests: {venue.maxGuests}
                     </p>
                 </div>
             </div>
             <div className="px-6 pt-4 pb-2">
-                <span className="inline-block rounded-full px-3 py-1 text-sm">
+                <p className="inline-block rounded-full text-sm">
                     {venue.price} NOK/night
-                </span>
+                </p>
             </div>
         </div>
     );
