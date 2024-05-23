a86c6fc56dc002be2ebda84b5efc2d6af2bd2e80
maleneivy <68667843+maleneivy@users.noreply.github.com>
2024-05-23 13:57:51 +0200

    Deleted _document and added fonts to layout.js, added .next to netlify.toml

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,17 +3,17 @@ import Navbar from '@/components/Navbar/Navbar';
 import { AuthProvider } from './lib/authProvider';
 
 export const metadata = {
-  title: {
-    template: '%s | Holidaze',
-    default: 'Holidaze',
-  },
-  description: 'Book your next vacation here',
+  title: 'Holidaze - Book your next vacay',
+  description: 'An accommodation booking site',
 };
 
 export default function RootLayout({ children }) {
   return (
     <AuthProvider>
       <html lang="en" style={{ width: '100%', overflowX: 'hidden' }}>
+        <head>
+          <link rel="stylesheet" href="https://use.typekit.net/uwn0qvz.css" />
+        </head>
         <body className="overflow-x-hidden">
           <Navbar />
           {children}
