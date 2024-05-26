2c58d9d53d4d2309d2d6d6dbd12f7829a0b55d37
maleneivy <68667843+maleneivy@users.noreply.github.com>
2024-05-26 22:16:45 +0200

    Added meta icons in VenueCard and in filtersearch

--- src/components/Filters/MetaFilter.jsx
+++ src/components/Filters/MetaFilter.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
+import { Icon } from '@/utils/icons';
 
 const MetaFilter = ({ onMetaChange, meta }) => {
   const [currentMeta, setCurrentMeta] = useState(meta);
@@ -16,6 +17,25 @@ const MetaFilter = ({ onMetaChange, meta }) => {
     onMetaChange(updatedMeta);
   };
 
+  const metaMap = {
+    wifi: {
+      label: 'Wifi',
+      icon: <Icon name="wifi" className="text-lg text-primary" />,
+    },
+    parking: {
+      label: 'Parking',
+      icon: <Icon name="parking" className="text-lg text-primary" />,
+    },
+    pets: {
+      label: 'Pets allowed',
+      icon: <Icon name="pets" className="text-lg text-primary" />,
+    },
+    breakfast: {
+      label: 'Breakfast included',
+      icon: <Icon name="breakfast" className="text-lg text-primary" />,
+    },
+  };
+
   return (
     <div className="mt-4 flex flex-col space-y-2">
       {Object.keys(currentMeta).map((key) => (
@@ -27,8 +47,11 @@ const MetaFilter = ({ onMetaChange, meta }) => {
             onChange={handleCheckboxChange}
             className="size-5"
           />
+          {metaMap[key] && metaMap[key].icon}
           <span className="text-lg">
-            {key.charAt(0).toUpperCase() + key.slice(1)}
+            {metaMap[key]
+              ? metaMap[key].label
+              : key.charAt(0).toUpperCase() + key.slice(1)}
           </span>
         </label>
       ))}
--- src/components/VenueCard/VenueCard.jsx
+++ src/components/VenueCard/VenueCard.jsx
@@ -1,5 +1,6 @@
 import ImageCarousel from '../ImageCarousel/ImageCarousel';
 import BaseButton from '../BaseButton/BaseButton';
+import { Icon } from '@/utils/icons';
 
 const VenueCard = ({ venue }) => {
   const defaultImage = '/default-post-image.jpg';
@@ -9,23 +10,62 @@ const VenueCard = ({ venue }) => {
       ? venue.media
       : [{ url: defaultImage, alt: 'Default Image' }];
 
+  const metaMap = {
+    wifi: {
+      label: 'Wifi',
+      icon: <Icon name="wifi" className="mr-2 text-lg text-primary" />,
+    },
+    parking: {
+      label: 'Parking',
+      icon: <Icon name="parking" className="mr-2 text-lg text-primary" />,
+    },
+    pets: {
+      label: 'Pets allowed',
+      icon: <Icon name="pets" className="mr-2 text-lg text-primary" />,
+    },
+    breakfast: {
+      label: 'Breakfast included',
+      icon: <Icon name="breakfast" className="mr-2 text-lg text-primary" />,
+    },
+  };
+
+  const availableMeta = Object.keys(venue.meta).filter(
+    (key) => venue.meta[key]
+  );
+
   return (
-    <div className="w-full max-w-sm rounded shadow-lg">
-      <ImageCarousel images={images} />
-      <div className="px-6 py-4">
-        <div className="mb-2 text-xl font-bold">
-          <h3 className="max-h-10 overflow-hidden truncate text-base">
-            {venue.name}
-          </h3>
-          <p className="text-sm">Max guests: {venue.maxGuests}</p>
+    <div className="flex w-full max-w-sm flex-col justify-between rounded shadow-lg">
+      <div>
+        <ImageCarousel images={images} />
+        <div className="px-6 py-4">
+          <div className="mb-2 text-xl font-bold">
+            <h3 className="max-h-10 overflow-hidden truncate text-base">
+              {venue.name}
+            </h3>
+            <p className="text-sm">Max guests: {venue.maxGuests}</p>
+            <div>
+              <ul className="mt-4 flex flex-col">
+                {availableMeta.map((key) => (
+                  <li
+                    key={key}
+                    className="flex items-center space-x-1 text-base font-normal"
+                  >
+                    {metaMap[key].icon}
+                    <span>{metaMap[key].label}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="px-6 pb-2 pt-4">
+          <p className="inline-block rounded-full text-sm">
+            {venue.price} NOK/night
+          </p>
         </div>
       </div>
-      <div className="px-6 pb-2 pt-4">
-        <p className="inline-block rounded-full text-sm">
-          {venue.price} NOK/night
-        </p>
-      </div>
-      <div className="p-2 text-end">
+      <div className="flex-grow" />
+      <div className="flex items-end justify-end p-2 text-end">
         <BaseButton href={`/venue/${venue.id}`}>View</BaseButton>
       </div>
     </div>
