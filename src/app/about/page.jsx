5a9173ae80796f7e42ecdd43515d04590ee4b886
maleneivy <68667843+maleneivy@users.noreply.github.com>
2024-05-26 09:33:38 +0200

    cleaned up about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -9,8 +9,8 @@ const About = () => {
           src="/nature_image_by_burkard_meyendriesch.jpg"
           alt="Image of a house at the coast at sunset. By Burkard Meyendriesch"
           className="my-4 w-full"
-          width={1200} // specify the width of the image
-          height={800} // specify the height of the image
+          width={1200}
+          height={800}
         />
         <p className="mb-4 text-end">
           Image by:{' '}
@@ -22,26 +22,25 @@ const About = () => {
           </Link>
         </p>
       </div>
-      <h1 className="mb-4 text-3xl font-bold">About Holidaze</h1>
+      <h1>About Holidaze</h1>
       <p className="mb-2">
-        Welcome to <strong>Holidaze</strong>, an accommodation booking site
-        designed to make your travel planning easy and enjoyable. This project
-        is part of an exam to showcase the skills and knowledge acquired during
-        our studies.
+        Welcome to Holidaze, an accommodation booking site designed to make your
+        travel planning easy and enjoyable. This project is part of an exam to
+        showcase the skills and knowledge acquired during our studies.
       </p>
-      <h2 className="mb-2 mt-4 text-2xl font-semibold">Our Platform</h2>
+      <h2>Our Platform</h2>
       <p className="mb-2">Holidaze caters to two main user groups:</p>
       <ul className="mb-4 list-inside list-disc">
         <li>
-          <strong>Customers</strong> – Users who can browse and book various
+          <strong>Customers</strong> - Users who can browse and book various
           venues for their stay.
         </li>
         <li>
-          <strong>Venue Managers</strong> – Users who can create and manage
+          <strong>Venue Managers</strong> - Users who can create and manage
           venues, making them available for bookings by customers.
         </li>
       </ul>
-      <h2 className="mb-2 mt-4 text-2xl font-semibold">Key Features</h2>
+      <h2>Key Features</h2>
       <ul className="mb-4 list-inside list-disc">
         <li>Easy to use interface for both customers and venue managers.</li>
         <li>
@@ -57,23 +56,21 @@ const About = () => {
           bookings.
         </li>
       </ul>
-      <h2 className="mb-2 mt-4 text-2xl font-semibold">Our Goal</h2>
+      <h2>Our Goal</h2>
       <p className="mb-2">
         Our aim is to provide a user-friendly platform that simplifies the
         process of booking accommodations. Whether you&apos;re a traveler
         looking for a place to stay or a venue manager wanting to list your
         property, Holidaze is designed to meet your needs.
       </p>
-      <h2 className="mb-2 mt-4 text-2xl font-semibold">Mobile-First Design</h2>
+      <h2>Mobile-First Design</h2>
       <p>
         Holidaze is built with a mobile-first view, ensuring that users have a
         smooth and intuitive experience regardless of the device they are using.
         This approach helps us create a responsive and accessible platform for
         all users.
       </p>
-      <h2 className="mb-2 mt-4 text-2xl font-semibold">
-        Want to become a customer/manager?
-      </h2>
+      <h2>Want to become a customer/manager?</h2>
       <Link
         href="/register"
         className="rounded p-2 underline hover:bg-lightGreen"
